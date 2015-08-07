67d633f7f251dc34ddf2746594e33fd7400ae4cb
ThotAlion <thomas.peyruse@gmail.com>
2015-08-07 23:53:57 +0200

    Interface tree GUI creation

--- FIRE.py
+++ FIRE.py
@@ -2,11 +2,43 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 import sys
 
-class FireGui(QMainWindow):
+class FireGui(QWidget):
     def __init__(self):
-        self = QMainWindow()
-        self.setStyleSheet("background-color: black;")
+        QWidget.__init__(self)
+        self.setStyleSheet("background-color: black; color: yellow;")
+        self.main_layout = QHBoxLayout()
+        self.setLayout(self.main_layout)
+        self.treesLayout = QVBoxLayout()
+        self.setLayout.addLayout(self.treesLayout)
+        self.interfaceTree = QTreeView()
+        self.main_layout.addWidget(self.interfaceTree)
+        self.showMaximized()
 
+# widget to control FIRE Interface list
+class InterfaceWidget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setStyleSheet("background-color: black; color: yellow;")
+        # list of components:
+        self.addButton = QPushButton("Add")
+        self.removeButton = QPushButton("Remove")
+        self.moveUpButton = QPushButton("Move up")
+        self.moveDnButton = QPushButton("Move dn")
+        self.tree = QTreeView()
+        
+        # organise the components in layouts
+        buttonslayout = QHBoxLayout()
+        buttonslayout.addWidget(self.addButton)
+        buttonslayout.addWidget(self.removeButton)
+        buttonslayout.addWidget(self.moveUpButton)
+        buttonslayout.addWidget(self.moveDnButton)
+        mainlayout = QVBoxLayout()
+        self.setLayout(mainlayout)
+        mainlayout.addLayout(buttonslayout)
+        mainlayout.addWidget(self.tree)
+        
+        self.show()
 app = QApplication(sys.argv)
-w = FireGui()
+w = InterfaceWidget()
+sys.exit(app.exec_())
 
