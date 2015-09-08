b3bf2364b7a5481420184a0c6a6ada9852438620
ThotAlion <thomas.peyruse@gmail.com>
2015-09-08 23:54:59 +0200

    Poses and objective interface are OK

--- FIRELIB2/Recorder.py
+++ FIRELIB2/Recorder.py
@@ -4,11 +4,11 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from Connexion import *
 
-class Objectives(QAbstractItemModel):
+class Objectives(QAbstractTableModel):
     
     def __init__(self,objectiveList = []):
         QAbstractItemModel.__init__(self)
-        self.setHorizontalHeaderLabels(["Name","Nature","Consign"])
+        self.headerdata = ["Name","Nature","Consign"]
         self.objectiveList = objectiveList
         
     def data(self,i,role):
@@ -23,14 +23,18 @@ class Objectives(QAbstractItemModel):
                     return obj["nature"]
                 elif i.column() == 2:
                     return obj["consign"]
-                
+    
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.headerdata[col]
+            
     def setData(self,i,value,role):
         if i.isValid():
             if role == Qt.EditRole:
                 if i.column() == 1:
                     self.objectiveList[i.row()]["nature"] = str(value.toString())
                 if i.column() == 2:
-                    self.objectiveList[i.row()]["consign"] = value.toDouble()
+                    self.objectiveList[i.row()]["consign"] = value.toDouble()[0]
                 return True
         else:
             return False
@@ -44,47 +48,54 @@ class Objectives(QAbstractItemModel):
             return f
     def rowCount(self,parent=QModelIndex()):
         return len(self.objectiveList)
+        
+    def columnCount(self,parent=QModelIndex()):
+        return 3
             
-class Poses(QAbstractItemModel):
+class Poses(QAbstractTableModel):
     
     def __init__(self,poseList = []):
         QAbstractItemModel.__init__(self)
-        #self.setHorizontalHeaderLabels(["Name","duration"])
-        self.setColumnCount(2)
+        self.headerdata = ["Name","duration"]
         self.poseList = poseList
         
     def data(self,i,role):
-        return "toto"
-        # if not i.isValid():
-            # return QVariant()
-        # else:
-            # obj = self.poseList[i.row()]
-            # if role == Qt.DisplayRole or role == Qt.EditRole:
-                # if i.column() == 0:
-                    # return obj["name"]
-                # elif i.column() == 1:
-                    # return str(obj["duration"])
-                
-    # def setData(self,i,value,role):
-        # if i.isValid():
-            # if role == Qt.EditRole:
-                # if i.column() == 1:
-                    # self.poseList[i.row()]["duration"] = str(value.toString())
-                # return True
-        # else:
-            # return False
+        if not i.isValid():
+            return QVariant()
+        else:
+            obj = self.poseList[i.row()]
+            if role == Qt.DisplayRole or role == Qt.EditRole:
+                if i.column() == 0:
+                    return obj["name"]
+                elif i.column() == 1:
+                    return str(obj["duration"])
+    
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.headerdata[col]
+    
+    def setData(self,i,value,role):
+        if i.isValid():
+            if role == Qt.EditRole:
+                if i.column() == 1:
+                    self.poseList[i.row()]["duration"] = value.toDouble()[0]
+                return True
+        else:
+            return False
     
     def flags(self,i):
-        return Qt.ItemIsEnabled | Qt.ItemIsSelectable
-        # if i.isValid():
-            # if i.column() in [0]:
-                # f = Qt.ItemIsEnabled | Qt.ItemIsSelectable
-            # if i.column() in [1]:
-                # f = Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
-            # return f
+        if i.isValid():
+            if i.column() in [0]:
+                f = Qt.ItemIsEnabled | Qt.ItemIsSelectable
+            if i.column() in [1]:
+                f = Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
+            return f
             
     def rowCount(self,parent=QModelIndex()):
         return len(self.poseList)
+        
+    def columnCount(self,parent=QModelIndex()):
+        return 2
 
 class Recorder(Block.Block,QWidget):
     """ this class describes a recorder """
@@ -104,7 +115,7 @@ class Recorder(Block.Block,QWidget):
         
         # creation of components
         self.objTable = QTableView()
-        self.objTable.setModel(self.objectiveList)
+        
         self.poseTable = QTableView()
         self.poseTable.setModel(self.poseList)
         self.tapeTable = QTableView()
@@ -126,22 +137,26 @@ class Recorder(Block.Block,QWidget):
     
         # signals
         self.connect(self.bInsertPose,SIGNAL("pressed()"),self.insertPose)
-    
-    
+        self.connect(self.poseTable,SIGNAL("clicked(QModelIndex)"),self.updateObjective)
+
     def insertPose(self):
         pose = {}
         pose["name"] = "toto"
         pose["duration"] = 1
-        pose["objectives"] = Objectives()
+        pose["objectives"] = Objectives([])
         for ch in self.inputs:
             obj = {}
             obj['name'] = ch
             obj['nature'] = 'L'
             obj['consign'] = self.robot[ch]
             pose["objectives"].objectiveList.append(obj)
+        print pose["objectives"].objectiveList
+        self.poseList.emit(SIGNAL("layoutAboutToBeChanged()"))
         self.poseList.poseList.append(pose)
-        #self.poseList.reset()
-        print self.poseList.poseList
+        self.poseList.emit(SIGNAL("layoutChanged()"))
+        
+    def updateObjective(self,i):
+        self.objTable.setModel(self.poseList.poseList[i.row()]["objectives"])
 
     
     def start(self):
