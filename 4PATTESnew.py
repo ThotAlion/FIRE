3a37fd98cb8fad6f6bbefb886628d0458d4d33fc
ThotAlion <thomas.peyruse@gmail.com>
2015-12-14 13:59:44 +0100

    New 4 legged walking

--- 4PATTESnew.py
+++ 4PATTESnew.py
@@ -23,7 +23,7 @@ members["right leg"] = ['r_hip_x','r_hip_z','r_hip_y','r_knee_y','r_ankle_y']
 interfaces = FIRELIB.Group.Group()
 interfaces.inputs["activate"].connectedTo = "1"
 
-interfaces.children['Poppy'] = FIRELIB.Robot.Robot(members,"10.0.0.4","8080")
+interfaces.children['Poppy'] = FIRELIB.Robot.Robot(members,"10.0.2.1","8080")
 interfaces.children['Poppy'].inputs["activate"].connectedTo = "1"
 interfaces.children['Poppy'].outputs["Temperature"].connectedTo = " TC "
 interfaces.children['Poppy'].inputs['Number'].connectedTo = " n "
@@ -32,13 +32,6 @@ for member in members:
     for art in members[member]:
         interfaces.children['Poppy'].inputs[art].connectedTo = " "+art+" "
         interfaces.children['Poppy'].outputs[art].connectedTo = " "+art+"m "
-        
-interfaces.children['pad'] = FIRELIB.xboxPad.xboxPad(0)
-interfaces.children['pad'].inputs["activate"].connectedTo = "1"
-interfaces.children['pad'].outputs['button A'].connectedTo = " buttonA "
-interfaces.children['pad'].outputs['button B'].connectedTo = " buttonB "
-interfaces.children['pad'].outputs['hat Y1'].connectedTo = " Y "
-interfaces.children['pad'].outputs['hat X1'].connectedTo = " X "
 
 systems = FIRELIB.Group.Group()
 systems.inputs["activate"].connectedTo = "1"
@@ -50,14 +43,24 @@ systems.children['Machine'].inputs["buttonB"].connectedTo = " buttonB "
 systems.children['Machine'].inputs["avant"].connectedTo = " Y == 1"
 systems.children['Machine'].inputs["gauche"].connectedTo = " X == -1"
 systems.children['Machine'].inputs["droite"].connectedTo = " X == 1"
-systems.children['Machine'].outputs["CSV"].connectedTo = " CSV "
+systems.children['Machine'].outputs["CSVPath"].connectedTo = " CSVPath "
 
-# tapes declaration
+
+
+
 systems.children['Player'] = FIRELIB.CSVPlayer.CSVPlayer(members)
-systems.children['Player'].inputs["activate"].connectedTo = " b_retourne "
-systems.children['Player'].outputs["finished"].connectedTo = " f_retourne "
+systems.children['Player'].inputs["activate"].connectedTo = "1"
+systems.children['Player'].inputs["Tape"].connectedTo = " CSVPath "
+systems.children['Player'].outputs["finished"].connectedTo = " finished "
 systems.children['Player'].outputs['Duration'].connectedTo = " Delta "
 systems.children['Player'].outputs['Number'].connectedTo = " n "
+
+systems.children['Disp'] = FIRELIB.Display.Display(["tape name","finished","n"])
+systems.children['Disp'].inputs["activate"].connectedTo = "1"
+systems.children['Disp'].inputs["tape name"].connectedTo = " CSVPath "
+systems.children['Disp'].inputs["finished"].connectedTo = " finished "
+systems.children['Disp'].inputs["n"].connectedTo = " n "
+
 for member in members:
     for art in members[member]:
         systems.children['Player'].outputs[art].connectedTo = " "+art+" "
